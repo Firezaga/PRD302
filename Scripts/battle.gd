00f3b8b35d5d31c9b709f9b6a1f03b8468a1c069
Firezaga <106500958+Firezaga@users.noreply.github.com>
2024-06-23 11:54:01 +1000

    Battle Text Log

--- Scripts/battle.gd
+++ Scripts/battle.gd
@@ -1,5 +1,6 @@
 extends CanvasLayer
 
+var log_array = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,12 +12,22 @@ func _process(delta):
 	pass
 
 #region DEBUG
-
+func _on_button_pressed():
+	await log_process_text("TEST TEST TEST")
 #endregion
 
 
 #region Text Log
-
+func log_process_text(text):
+	$Log/Label.text = ""
+	if log_array.size() == 14:
+		log_array.pop_front()
+	log_array.push_back(text)
+	for i in log_array:
+		$Log/Label.text += i + "\n"
+	for i in len(text):
+		$Log/Label.text += text[i]
+		await get_tree().create_timer(0.01).timeout
 #endregion
 
 
@@ -33,3 +44,6 @@ func _process(delta):
 #region CORE Functions
 
 #endregion
+
+
+
