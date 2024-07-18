73913810724993ececca58d1af3a513db256bc8e
William Hendarto <40332462+Damstructions@users.noreply.github.com>
2024-07-18 12:37:52 +1000

    Went straight to implement tier 2, Alcora can finally gamble

--- Scripts/Companions/alcora.gd
+++ Scripts/Companions/alcora.gd
@@ -15,29 +15,54 @@ var T1A1_name = "Boost ATK"
 var T1A1_log = "Alcora is boosting attack"
 var T1A1_cost = 5
 var T1A1_is_attack = true
+func t1_a1(base):
+	return base + Strength
 
 var T1A2_unlock = true
 var T1A2_name = "T1A2"
 var T1A2_log = "T1A2"
 var T1A2_cost = 5
 var T1A2_is_attack = true
+func t1_a2(base):
+	return base + Strength
 
 var T1A3_unlock = true
 var T1A3_name = "T1A3"
 var T1A3_log = "T1A3"
 var T1A3_cost = 5
 var T1A3_is_attack = true
+func t1_a3(base):
+	return base + Strength
 
-var T2A1_name = "T2A1"
-var T2A1_log = "T2A1"
-var T2A1_cost = 5
+var T2A1_name = "Coin Flip"
+var T2A1_log = "Alcora flips a coin..."
+var T2A1_cost = 15
 var T2A1_is_attack = true
+func t2_a1(base):
+	var coinflip = (randi()%2)
+	if coinflip == 1:
+		return base + (Strength*10)
+		#print success!
+	else:
+		return base
+		#print fail sadge
 
 var T2A2_unlock = true
-var T2A2_name = "Placeholde"
-var T2A2_log = "Placeholde"
-var T2A2_cost = 5
+var T2A2_name = "Full House"
+var T2A2_log = "Alcora bets high, and prays for fortune!"
+var T2A2_cost = 15
 var T2A2_is_attack = true
+func t2_a2(base):
+	var AlcoraT2A2chance = (randi()%100)
+	if AlcoraT2A2chance == range(0,19):
+		return base + (Strength*20)
+		#Alcora gets what she wants!
+	elif AlcoraT2A2chance == range(20,60):
+		return base + (Strength*2)
+		#Alcora lost the bet, but got some back!
+	else:
+		return base + (Strength*0.1)
+		#Alcora completely lost the bet...
 
 var T2A3_unlock = true
 var T2A3_name = "Placehoder"
@@ -88,21 +113,6 @@ func _ready():
 func _process(delta):
 	pass
 
-func t1_a1(base):
-	return base + Strength
-
-func t1_a2(base):
-	return base + Strength
-
-func t1_a3(base):
-	return base + Strength
-
-func t2_a1(base):
-	return base + Strength
-
-func t2_a2(base):
-	return base + Strength
-
 func t2_a3(base):
 	return base + Strength
 
@@ -123,3 +133,4 @@ func t4_a2(base):
 
 func t4_a3(base):
 	return base + Strength
+	
