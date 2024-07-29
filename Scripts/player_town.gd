7d949883849c435f6071a499145a0996b261a915
William Hendarto <40332462+Damstructions@users.noreply.github.com>
2024-07-29 16:51:26 +1000

    broken anim (velocity is my big suspect)

--- Scripts/player_town.gd
+++ Scripts/player_town.gd
@@ -1,18 +1,25 @@
 extends CharacterBody2D
 
 @export var speed = 1000;
+@onready var animation_tree = $AnimationTree
 
 func _process(delta):
 	if !Global.PlayerMove:
 		return
 	velocity = Vector2.ZERO
+	#input
 	if Input.is_action_pressed("right"):
 		velocity.x += 1
 		$Sprites.scale.x = 0.3
+		isMoving(true)
 	if Input.is_action_pressed("left"):
 		velocity.x -= 1
 		$Sprites.scale.x = -0.3
-	
+		isMoving(true)
+	else:
+		isMoving(false)
+	update_blend_position()
+
 	#if Input.is_action_pressed("up"):
 		#velocity.y -= 1
 	#if Input.is_action_pressed("down"):
@@ -23,6 +30,12 @@ func _process(delta):
 	
 	move_and_slide()
 
+func isMoving(value):
+	animation_tree["parameters/conditions/is_running"] = value
+	animation_tree["parameters/conditions/is_not_running"] = not value
+	
+func update_blend_position():
+	animation_tree["parameters/Run/blend_position"] = velocity
 
 func _on_pause_button_pressed():
 	Global.PlayerMove = false
